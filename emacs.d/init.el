8498fc196015a28781be9da392315ba70f8bf440
Akihiro Hayashi <ahayashi@rice.edu>
2020-08-09 18:31:41 -0400

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,7 +41,23 @@
               tab-width 4
               indent-tabs-mode nil)
 (setq-default show-trailing-whitespace t)
-(setq c-offsets-alist '((inlambda . 0))) ; experimental
+
+;;;; indent for lambda expressions
+(if (>= emacs-major-version 26)
+    (setq c-offsets-alist '((inlambda . 0))) ; experimental
+  (defadvice c-lineup-arglist (around my activate)
+    "Improve indentation of continued C++11 lambda function opened as argument."
+    (setq ad-return-value
+          (if (and (equal major-mode 'c++-mode)
+                   (ignore-errors
+                     (save-excursion
+                       (goto-char (c-langelem-pos langelem))
+                       ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+                       ;;   and with unclosed brace.
+                       (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+              0                           ; no additional indent
+            ad-do-it)))                   ; default behavior
+  )
 
 ;; do not generate backup files
 (setq make-backup-files nil)
