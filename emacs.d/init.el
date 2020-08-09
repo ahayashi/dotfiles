977871db48b5e2ab899b9ab7be390fd04f75adaa
Akihiro Hayashi <ahayashi@rice.edu>
2020-08-08 22:48:57 -0400

    Update init.el (now use use-package.el)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,43 +10,36 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;; auto install
+;; package.el
 (require 'package)
-
-;; package repos
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
 (package-initialize)
 
-(unless package-archive-contents (package-refresh-contents))
+;; use-package.el
+(when (require 'use-package nil t)
+  ;; major-modes
+  (use-package cuda-mode :mode ("\\.cu\\'" . cuda-mode))
+  (use-package chpl-mode :mode ("\\.chpl\\'" . chpl-mode))
+  (use-package js2-mode :mode ("\\.js\\'" . js2-mode))
+  (use-package llvm-mode :mode ("\\.ll\\'" . llvm-mode))
+  (use-package scala-mode :mode ("\\.scala\\'" . scala-mode))
+  (use-package shell-script-mode :mode ("dotzshrc" . shell-script-mode) ("dotzshenv" . shell-script-mode))
+  (use-package rust-mode :mode ("\\.rs\\'" . rust-mode))
+  ;; mode-line (filename)
+  (use-package uniquify
+    :config
+    (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+    (setq uniquify-ignore-buffers-re "*[^*]+*"))
+  )
 
-;;; On-demand installation of packages (by @purcell)
-(defun require-package (package &optional min-version no-refresh)
-  "Install given PACKAGE, optionally requiring MIN-VERSION. If
-NO-REFRESH is non-nil, the available package lists will not be
-re-downloaded in order to locate PACKAGE."
-  (if (package-installed-p package min-version) t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package)
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
-
-(require-package 'csv-mode)
-(require-package 'js2-mode)
-(require-package 'json-mode)
+;; delete -> bs
+(normal-erase-is-backspace-mode 0)
 
 ;; do not generate backup files
 (setq make-backup-files nil)
 (setq delete-auto-save-files t)
 
-;; delete -> bs
-(normal-erase-is-backspace-mode 0)
-
 ;; when saving
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 
 ;; remove menubar
@@ -60,15 +53,10 @@ re-downloaded in order to locate PACKAGE."
 (setq display-time-string-forms '(month "/" day " (" dayname ") " 12-hours ":" minutes am-pm))
 (display-time)
 
-;; mode-line (newline)
+;; mode-line (show newline)
 (setq eol-mnemonic-dos "(CRLF)")
 (setq eol-mnemonic-mac "(CR)")
 (setq eol-mnemonic-unix "(LF)")
-
-;; mode-line (filename)
-(when (require 'uniquify nil t)
-  (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-  (setq uniquify-ignore-buffers-re "*[^*]+*"))
 
 ;; scroll
 (setq scroll-conservatively 1)
@@ -83,7 +71,7 @@ re-downloaded in order to locate PACKAGE."
 (set-face-background 'show-paren-match "grey66")
 
 ;; indent / spacing
-(setq-default tab-width 4 indent-tabs-mode nil)
+(setq-default tab-width 4 indent-tabs-mode nil c-basic-offset 4)
 
 ;; color (font-lock)
 (setq font-lock-support-mode 'jit-lock-mode)
@@ -103,23 +91,3 @@ re-downloaded in order to locate PACKAGE."
 ;; color (region)
 (set-face-background 'region "gray66")
 (set-face-foreground 'region "black")
-
-;; major mode (adding suffixes)
-(add-to-list 'auto-mode-alist (cons  "\\.\\(cu\\)\\'" 'c++-mode))
-(add-to-list 'auto-mode-alist (cons  "\\.\\(hj\\)\\'" 'java-mode))
-(add-to-list 'auto-mode-alist (cons  "dotzshrc" 'shell-script-mode))
-(add-to-list 'auto-mode-alist (cons  "dotzshenv" 'shell-script-mode))
-
-;; major mode (regiter new major modes)
-(when (require 'chpl-mode nil t)
-  (add-to-list 'auto-mode-alist (cons  "\\.\\(chpl\\)\\'" 'chpl-mode)))
-(when (require 'llvm-mode nil t)
-  (add-to-list 'auto-mode-alist (cons  "\\.\\(ll\\)\\'" 'llvm-mode)))
-(when (require 'scala-mode nil t)
-  (add-to-list 'auto-mode-alist (cons  "\\.\\(scala\\)\\'" 'scala-mode)))
-
-;; major mode (c-mode)
-(defun my-c-c++-mode-init () (setq c-basic-offset 4))
-
-(add-hook 'c-mode-hook 'my-c-c++-mode-init)
-(add-hook 'c++-mode-hook 'my-c-c++-mode-init)
